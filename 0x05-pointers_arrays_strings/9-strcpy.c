9667d896f456af3d465f1c639c9b457f0dfb56cf
ZooHairDev <zouhai.ob@gmail.com>
2023-02-28 00:41:54 +0100

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,26 +10,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int n = 0;
 	int i;
 
-	while (src[n] != '\0')
-	{
-		n++;
-	}
-
 	i = 0;
-	while (i < n && src[i] != '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-
-	i = i;
-	while (i <= n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+	dest[i] = '\0';
 	return (dest);
 }
