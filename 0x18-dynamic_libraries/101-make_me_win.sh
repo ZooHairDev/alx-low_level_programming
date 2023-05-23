c14e9fdf19d10f88e3995cef3b4d3bccb2216e61
Zouhair OB <46304063+ZooHairDev@users.noreply.github.com>
2023-05-23 18:32:16 +0100

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/ZooHairDev/alx-low_level_programming/tree/main/0x18-dynamic_libraries/lib101-makemewin.so
+export LD_PRELOAD=$HOME/lib101-makemewin.so
